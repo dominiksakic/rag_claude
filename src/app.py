ec1fc37cc8c82a720afcbcc8e043148145b2a177
dominiksakic <dominik.sakic@yahoo.com>
2025-01-03 19:36:07 +0900

    feat: use graph with memory to answer question

--- src/app.py
+++ src/app.py
@@ -2,9 +2,12 @@ from utils.chatbot import graph
 
 def stream_graph_updates(user_input: str, thread_id: str):
     config = {"configurable":{"thread_id": thread_id}}
-    for event in graph.stream({"messages": [("user", user_input)]}, config):
-        for value in event.values():
-            print("Assistant:", value["messages"][-1].content)
+    events = graph.stream(
+         {"messages": [("user", user_input)]}, config, stream_mode="values"
+    )
+    for event in events:
+        if "messages" in event:
+            print(event["messages"][-1].content)
 
 def main():
     while True:
@@ -15,11 +18,8 @@ def main():
                 break
 
             stream_graph_updates(user_input, "1")
-        except:
-            # fallback if input() is not available
-            user_input = "What do you know about LangGraph?"
-            print("User: " + user_input)
-            stream_graph_updates(user_input)
+        except Exception as e:
+            print(f"Error:{e}")
             break
 
 if __name__ == "__main__":
--- src/utils/chatbot.py
+++ src/utils/chatbot.py
@@ -3,6 +3,7 @@ from typing import Tuple, List, Annotated
 
 from typing_extensions import TypedDict
 
+from langchain_chroma import Chroma
 from langgraph.checkpoint.memory import MemorySaver
 from langgraph.graph import StateGraph, START, END
 from langgraph.graph.message import add_messages
@@ -15,8 +16,16 @@ APPCFG = LoadConfig()
 with open("config/app_config.yml") as cfg:
     app_config = yaml.load(cfg, Loader=yaml.FullLoader)
 
+# Load the embedding function
+embeddings = APPCFG.embedding_model
+
+# Load the DB
+vectordb = Chroma(persist_directory=APPCFG.persist_directory,
+                  embedding_function=embeddings)
+
 class State(TypedDict):
     messages:Annotated[list,add_messages]
+    retrieved_docs: List[str]
 
 graph_builder = StateGraph(State)
 
@@ -25,13 +34,23 @@ llm = ChatGoogleGenerativeAI(
         temperature = 0
 )
 
+def retrieve_documents(state: State):
+    last_message= state["messages"][-1]
+    question = last_message.content
+    question = "# user new question:\n" + question
+    docs = vectordb.similarity_search(question, k=APPCFG.k)
+    retrieved_docs = [str(doc.page_content) for doc in docs]
+    return {"retrieved_docs":retrieved_docs}
+
 def chatbot(state: State):
-    return {"messages":[llm.invoke(state["messages"])]}
+    return {"messages": [llm.invoke(state["messages"])]}
 
 memory = MemorySaver() # in Development, for production research sql memory
 
+graph_builder.add_node("retrieve_documents", retrieve_documents)
 graph_builder.add_node("chatbot", chatbot)
-graph_builder.add_edge(START, "chatbot")
+graph_builder.add_edge(START, "retrieve_documents")
+graph_builder.add_edge("retrieve_documents", "chatbot")
 graph_builder.add_edge("chatbot", END)
 graph = graph_builder.compile(checkpointer =memory)
 
